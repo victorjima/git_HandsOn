b6c805a09b9b762389763186073b0387616ab875
Víctor Jiménez Martínez <victor.jimenez@uvic.cat>
2022-02-28 11:11:16 +0100

    seqClass fixed

--- seqClass.py
+++ seqClass.py
@@ -15,19 +15,19 @@ args = parser.parse_args() # define the variable for the arguments
 
 args.seq = args.seq.upper()  # transform the sequence into capital letters
 if re.search('^[ACGTU]+$', args.seq): # search for all the letters in the sequence
-    if re.search('T', args.seq): 
+    if re.search('^[ACGT]+$', args.seq): 
         print ('The sequence is DNA') # if it contains a T the sequence is DNA
-    elif re.search('U', args.seq):
+    elif re.search('^[ACGU]+$', args.seq):
         print ('The sequence is RNA') # if it contains a U the sequence is RNA
     else:
-        print ('The sequence can be DNA or RNA') # if it contains both, either
+        print ('Ambiguity: the sequence may be either DNA or RNA') # if it contains both, either
 else:
-    print ('The sequence is not DNA nor RNA') # not identified
+    print ('The sequence is neither DNA nor RNA') # not identified
 
 if args.motif:
     args.motif = args.motif.upper() # transform motif to capital letters
-    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '') # message that it is running
+    print("Motif search enabled: looking for motif in sequence...") # message that it is running
     if re.search(args.motif, args.seq): 
-        print(f'Motif {args.motif} is FOUND in the sequence {args.seq}') # if the motif is found
+        print(f"Motif {args.motif}  is found in sequence {args.seq}") # if the motif is found
     else:
-        print(f'Motif {args.motif} is not in sequence')
+        print(f"Motif {args.motif} is not found in sequence {args.seq}")
